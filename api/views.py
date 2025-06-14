0dfc953055a860b7cbb58679f8a8e78224605305
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-14 11:16:49 +0200

    Refactor welcome view to use WelcomeService for dynamic welcome message

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
+from .v1.services.WelcomeService import WelcomeService
 
 @api_view(['GET'])
 def welcome(request):
-    return JsonResponse({"message": "Welcome to the API!"})
+    welcome_service = WelcomeService()
+    welcome_message = welcome_service.get_welcome_message()
+    return JsonResponse({"message": welcome_message})
